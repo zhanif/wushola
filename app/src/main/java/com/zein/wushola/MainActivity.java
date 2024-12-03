6c9250f671c10266fc8adc90906bd442a96c7df0
zhanif <ihsanfauzan76@gmail.com>
2024-12-03 16:45:10 +0700

    refactor: remove the unecessary code, modularize the code

--- app/src/main/java/com/zein/wushola/MainActivity.java
+++ app/src/main/java/com/zein/wushola/MainActivity.java
@@ -5,26 +5,19 @@ import android.app.job.JobScheduler;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.zein.wushola.context.PrayerContext;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.concurrent.ExecutorService;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ExecutorService executorService;
     private PrayerContext prayerContext;
 
     @Override
@@ -42,21 +35,25 @@ public class MainActivity extends AppCompatActivity {
         Button syncButton = findViewById(R.id.sync_button);
 
         try {
-            syncButton.setOnClickListener(view -> {
-                prayerContext.reloadAPI("1223", 2024, 12);
-                Toast.makeText(this, "Berhasil mensinkronisasi jadwal sholat", Toast.LENGTH_SHORT).show();
-
-                Context context = getApplicationContext();
-                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-                ComponentName componentName = new ComponentName(context, MainWidget.class);
-
-                int[] appWidgetIds = appWidgetManager.getAppWidgetIds(componentName);
-                new MainWidget().refresh(context, appWidgetManager, appWidgetIds);
-            });
+            syncButton.setOnClickListener(handleSyncButtonOnClick());
         }
         catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private View.OnClickListener handleSyncButtonOnClick() {
+        return view -> {
+            prayerContext.reloadAPI("1223", 2024, 12);
+            Toast.makeText(this, "Berhasil mensinkronisasi jadwal sholat", Toast.LENGTH_SHORT).show();
+
+            Context context = getApplicationContext();
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName componentName = new ComponentName(context, MainWidget.class);
+
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(componentName);
+            new MainWidget().refresh(context, appWidgetManager, appWidgetIds);
+        };
     }
 
     private void scheduleUpdateForPrayerTimes() {
@@ -68,22 +65,15 @@ public class MainActivity extends AppCompatActivity {
             long prayerMillis = getMillisFromTimeString(prayerTime);
             long currentTimeMillis = getMillisFromTimeString(prayerContext.getCurrentTime());
             long triggerTime = prayerMillis - currentTimeMillis;
-
-            // Log trigger time to see if it's correct
-            System.out.println("Trigger time for " + prayer + ": " + triggerTime);
-
             if (triggerTime > 0) {
                 PersistableBundle extras = new PersistableBundle();
                 extras.putString("prayer", prayer);
 
-                // Create a job with the calculated trigger time
                 JobInfo jobInfo = new JobInfo.Builder(prayer.hashCode(), new ComponentName(MainActivity.this, PrayerJobService.class))
                         .setPersisted(true)
                         .setExtras(extras)
                         .setMinimumLatency(triggerTime)
                         .build();
-
-                // Schedule the job
                 jobScheduler.schedule(jobInfo);
             }
         }
@@ -103,8 +93,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (executorService != null && !executorService.isShutdown()) {
-            executorService.shutdown();
-        }
     }
 }
